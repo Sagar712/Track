577a24e9ac42f2d5ed78451aa1dfa3c7684c4ae6
Sagar Wankhade <wankhadesagar761@gmail.com>
2022-11-14 10:14:57 +0530

    service worker fixing

--- sw.js
+++ sw.js
@@ -1,11 +1,14 @@
+let collectin_name =  "TractMe"
+const assets = ["./", "./main.js", "./style.css", "./Sparrow.png", "./editMode.html","./edit.js",
+"./contact-us/contact.html", "./contact-us/otherstyle.css", 
+"./import-export/import-export.css", "./import-export/import-export.html", "./import-export/import-export.js", 
+"./analytics/style.css", "./analytics/subjects.json", "./analytics/app.js", 
+"./analytics/analytics.html", "./analytics/suggest.js", "./analytics/suggest_operation.js"]
+
 self.addEventListener("install", e => {
     e.waitUntil(
-        caches.open("TractIt").then(cache => {
-            return cache.addAll(["./", "./main.js", "./style.css", "./Sparrow.png", "./editMode.html","./edit.js",
-            "./contact-us/contact.html", "./contact-us/otherstyle.css", 
-            "./import-export/import-export.css", "./import-export/import-export.html", "./import-export/import-export.js", 
-            "./analytics/style.css", "./analytics/subjects.json", "./analytics/app.js", 
-            "./analytics/analytics.html", "./analytics/suggest.js", "./analytics/suggest_operation.js"]);
+        caches.open(collectin_name).then(cache => {
+            return cache.addAll(assets);
         })
     );
 });
